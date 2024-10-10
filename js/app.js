82da667d3ee6387b4a266226eed8d5cdedaea762
Nathaniel Bugeja <nathanielbugeja15@gmail.com>
2024-10-10 22:30:30 +0200

    Committing new changes
    Making some changes to improve the code and making it more efficient

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('show');
+        }
+        else
+        {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader-hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    })
+})
